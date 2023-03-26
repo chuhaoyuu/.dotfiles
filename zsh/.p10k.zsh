854088487af6a4277e55b4f9a7c576d4fc583a09
Jacky Chu <chuhaoyuu@gmail.com>
2023-03-27 00:07:13 +0800

    Update opacity and prompt symbols in configuration files

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -209,8 +209,7 @@
   # Red prompt symbol if the last command failed.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS,VIOWR}_FOREGROUND=196
   # Default prompt symbol.
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_VIINS_CONTENT_EXPANSION='👉'
-  typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_VIINS_CONTENT_EXPANSION='⛔'
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='➔ '
   # Prompt symbol in command vi mode.
   typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='➔ '
   # Prompt symbol in visual vi mode.
